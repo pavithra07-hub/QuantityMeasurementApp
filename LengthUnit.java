506191def10b12515e1d157a16687363a73c70e4
pavithra <pavithrasanthoshi07@gamil.com>
2026-04-22 10:49:06 +0530

    UC8: Refactoring Unit Enum to Standalone with Conversion Responsibility

---
+++ LengthUnit.java
@@ -0,0 +1,20 @@
+public enum LengthUnit {
+    FEET(1.0),
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
+
+    private final double toFeetFactor;
+
+    LengthUnit(double toFeetFactor) {
+        this.toFeetFactor = toFeetFactor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * toFeetFactor; // → feet
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / toFeetFactor;
+    }
+}
